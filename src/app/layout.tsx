b0993f1b1fea51e79641bddea3de01fd399c7530
thenoobchad <noobchaddy@gmail.com>
2025-04-03 23:06:51 +0100

    display images

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,12 +14,25 @@ const geist = Geist({
   variable: "--font-geist-sans",
 });
 
+function TopNav() {
+  return (
+    <nav className="flex w-full items-center justify-between border-b p-4 text-lg font-semibold">
+      <div>Gallery</div>
+
+      <div>Sign In</div>
+    </nav>
+  );
+}
+
 export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en" className={`${geist.variable}`}>
-      <body>{children}</body>
+    <html lang="en" className={`${geist.variable} `}>
+      <body className="flex flex-col gap-4">
+        <TopNav />
+        {children}
+      </body>
     </html>
   );
 }
